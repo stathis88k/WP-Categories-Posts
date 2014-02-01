59fc08ccbc6cfc3d75b81e8d0e63e443f2c9f60d
stathis88k <stathis.kapaniaris@gmail.com>
2014-02-01 14:09:49 +0100

    Update wp-categories.php

--- wp-categories.php
+++ wp-categories.php
@@ -18,37 +18,67 @@
            exit ($exit_msg);
       }
      
+      //specify the shortcode and the function that it will be used
       add_shortcode('wp-categories','listPosts');
+      
+      //create the method
       function listPosts($attr){
+          /* Turn on the output buffering
+            We use this so the output of the shortcode will appear in the place we want.
+            Without this the output of the function will appear always on the top of the content
+            Alternatively you can return the content of the method as a string */
           ob_start();
+          
+          //Extract the variables given in the short code so we can use them as independent variables
           extract($attr);
+          
+          //This line is used so we can display the posts in multiple pages if 
+          //they are more than a specific number we define in the nest line
           $page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+          
+          //get the posts from the category we asked.
+          //Set also how many posts you want to display in one page
           query_posts("cat=$cat&posts_per_page=10&paged=$page");
+          
           if (have_posts()) :
-          while (have_posts()) : the_post();  ?>
-               
-                    <div class="date_title">
-                        <div class="date_back">
-                            <div class="month"><?php the_time('M') ?></div>
-                            <div class="day"><?php the_time('d') ?></div>               
-                        </div>                       
-                        <h2><a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                    </div>      
+          while (have_posts()) : the_post();  
+          
+          /* 
+          The following part inside the while loop is how you are going to display each post on the list
+          This is up to what you want to display and how. Here I give a basic example of what I did.
+          */
+           ?>
+             <div class="date_title">
+                  <div class="date">
+                   <div class="month"><?php the_time('M') ?></div>
+                      <div class="day"><?php the_time('d') ?></div>               
+                   </div>                       
+                  <a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+            </div>      
                       
-        <?php if ( has_post_thumbnail() ) {
-            $mypostimage = the_post_thumbnail( 'eventthumb', array('class' => 'eventthumb' , 'alt' => get_the_title() , 'title' => get_the_title() ));
-            the_excerpt();} else { echo'<a href="'.get_permalink().'">'.category_image($post->ID,'medium').'</a>';
-            the_excerpt();  } ?>    
+        <?php the_excerpt();  } ?>    
             <div style="clear:both;"></div>
-            <hr><br><br>
+            <hr>
+        <?php
+               
+          // end the while loop
+        
+         endwhile;
                 
-               <?php
-                endwhile;?>
-                <div class="navigation"><p><?php posts_nav_link('&#8734;','Next &raquo;&raquo;',' &laquo;&laquo; Previous'); ?></p></div>
-                <?php else : ?>
-            <?php endif; ?>
-                
-                
-                
+         //Add the navigation buttons if the posts are more than one page
+       ?>
             
-     <?php wp_reset_query(); return ob_get_clean(); } ?>
+       <div class="navigation"><p><?php posts_nav_link('&#8734;','Next &raquo;&raquo;',' &laquo;&laquo; Previous'); ?></p></div>
+      <?php
+          //do something else if the category has no posts
+          else : ?>
+      <?php endif; ?>
+
+     <?php
+     
+     // reset the query so you will not get something more. Otherwise the content of the posts will be displayed after
+      wp_reset_query(); 
+      
+      //finally return the buffer contents and delete the output buffer
+      return ob_get_clean(); 
+} // end of the function ?>
